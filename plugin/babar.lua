2b64d87b65e94563c1b29eb122c8dbef2e7771a8
Heber <herbermqh@gmail.com>
2021-10-08 18:20:56 -0400

    change icon close

--- plugin/babar.lua
+++ plugin/babar.lua
@@ -35,7 +35,7 @@ vim.g.bufferline = {
   -- Configure icons on the bufferline.
   icon_separator_active = ' ',
   icon_separator_inactive = '',
-  icon_close_tab = '',
+  icon_close_tab = '',
   icon_close_tab_modified = '',
   icon_pinned = ' ',
 
