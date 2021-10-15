985b5ce56e5a6038878827fa62205ae35cf2610b
Heber <herbermqh@gmail.com>
2021-10-15 14:08:04 -0400

    config nvim-tree

--- plugin/packer.lua
+++ plugin/packer.lua
@@ -57,9 +57,9 @@ require('packer').startup(function(use)
         }
     })
     use({
-    'kyazdani42/nvim-tree.lua',
-    requires = 'kyazdani42/nvim-web-devicons',
-    -- config = function() require'nvim-tree'.setup {} end
+      'kyazdani42/nvim-tree.lua',
+      requires = 'kyazdani42/nvim-web-devicons',
+      config = function() require'nvim-tree'.setup {} end
     })
 
 
