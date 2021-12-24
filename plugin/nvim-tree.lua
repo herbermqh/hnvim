0b6a3bb25d78693134c1026292a809fb18ba2f39
Heber <herbermqh@gmail.com>
2021-12-24 15:03:31 -0400

    varios

--- plugin/nvim-tree.lua
+++ plugin/nvim-tree.lua
@@ -124,15 +124,15 @@ require'nvim-tree'.setup {
         { key = "I",                            cb = tree_cb("toggle_ignored") },
         { key = "H",                            cb = tree_cb("toggle_dotfiles") },
         { key = "R",                            cb = tree_cb("refresh") },
-        { key = "a",                            cb = tree_cb("create") },
+        { key = "m",                            cb = tree_cb("create") },
         { key = "d",                            cb = tree_cb("remove") },
-        { key = "r",                            cb = tree_cb("rename") },
+        { key = "a",                            cb = tree_cb("rename") }, --Heber
         { key = "<C-r>",                        cb = tree_cb("full_rename") },
-        { key = "x",                            cb = tree_cb("cut") },
-        { key = "c",                            cb = tree_cb("copy") },
-        { key = "p",                            cb = tree_cb("paste") },
-        { key = "y",                            cb = tree_cb("copy_name") },
-        { key = "Y",                            cb = tree_cb("copy_path") },
+        { key = "dd",                            cb = tree_cb("cut") }, --
+        { key = "yy",                            cb = tree_cb("copy") }, --Heber
+        { key = "pp",                            cb = tree_cb("paste") }, --Heber
+        { key = "yn",                            cb = tree_cb("copy_name") }, --Heber
+        { key = "yd",                            cb = tree_cb("copy_path") }, --Heber
         { key = "gy",                           cb = tree_cb("copy_absolute_path") },
         { key = "[c",                           cb = tree_cb("prev_git_item") },
         { key = "]c",                           cb = tree_cb("next_git_item") },
