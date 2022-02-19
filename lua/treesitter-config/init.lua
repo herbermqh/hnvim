e3c6ad9c36d1960f60f30c821efc22f97186d1ae
Heber <herbermqh@gmail.com>
2022-02-19 10:02:44 -0400

    varios

--- lua/treesitter-config/init.lua
+++ lua/treesitter-config/init.lua
@@ -2,7 +2,7 @@ require'nvim-treesitter.configs'.setup{
   ensure_installed = {"python","lua","latex"},
   incremental_selection = { enable = false },
   highlight = {
-    -- enable = true,
+    enable = true,
     additional_vim_regex_highlighting = false,
   },
   indent = { enable = false },
