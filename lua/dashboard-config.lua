fda37b0b5fda757c50a65e64a8988a1c81e28fb4
Heber MQ <herbermqh@gmail.com>
2022-06-26 13:33:52 -0400

    dashboard config

--- lua/dashboard-config.lua
+++ lua/dashboard-config.lua
@@ -1,6 +1,7 @@
 local home = os.getenv('HOME')
 local db = require('dashboard')
 db.preview_command = 'cat | lolcat -F 0.3'
+db.preview_file_path = home .. '/.config/nvim/static/neovim.cat'
 db.preview_file_height = 12
 db.preview_file_width = 80
 db.custom_center = {
@@ -24,10 +25,10 @@ db.custom_center = {
       desc = 'Find  word                              ',
       action = 'Telescope live_grep',
       shortcut = 'SPC f w'},
-      -- {icon = '  ',
-      -- desc = 'Open Personal dotfiles                  ',
-      -- action = 'Telescope dotfiles path=' .. home ..'/.dotfiles',
-      -- shortcut = 'SPC f d'},
+      {icon = '  ',
+      desc = 'Open Personal dotfiles                  ',
+      action = 'Telescope dotfiles path=' .. home ..'/.dotfiles',
+      shortcut = 'SPC f d'},
     }
 
 
